986ccb02f74cbf9dc5aa821bb817c2a329ca59a9
iachella <iachella@student.chalmers.se>
2021-11-17 15:56:05 +0000

    Update appointment.js with suggested appointment structure from project description

---
+++ models/appoinment.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var appointmentSchema = new Schema({
+    userid: {type: int},
+    requestid: {type: int},
+    dentistid: {type: int},
+    issuance: {type: int},
+    date: {type: Date}
+});
+
+module.exports = mongoose.model('appointment', appointmentSchema);
