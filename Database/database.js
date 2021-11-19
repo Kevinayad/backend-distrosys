17e67d7a974d589a159a4c2d42cb5761daf9cd3a
iachella <iachella@student.chalmers.se>
2021-11-19 10:33:01 +0000

    add code to connect to database

---
+++ Database/database.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+const mongoURI = "mongodb+srv://team12user:team12developer@dit355team12cluster.bwr7a.mongodb.net/dentistimodb?retryWrites=true";
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+    if (err) {
+        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+        console.error(err.stack);
+        process.exit(1);
+    }
+    console.log(`Connected to MongoDB with URI: ${mongoURI}`);
+});
