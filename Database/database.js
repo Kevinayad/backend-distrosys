8b777e84df8bd6e74c90211a9b8e57739961d778
Kevinayad <kevinay@student.chalmers.se>
2021-11-25 07:21:59 +0100

    added json script please test

--- Database/database.js
+++ Database/database.js
@@ -1,12 +1,22 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose').mongoose;
 
 const mongoURI = "mongodb+srv://team12user:team12developer@dit355team12cluster.bwr7a.mongodb.net/dentistimodb?retryWrites=true";
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
     if (err) {
         console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
         console.error(err.stack);
         process.exit(1);
     }
     console.log(`Connected to MongoDB with URI: ${mongoURI}`);
+});
+mongoose.connect('dentistRepo.json', function (err, db) {
+    if (err) throw err;
+    var jsonData = db;
+    var jsonParsed = JSON.parse(jsonData);
+    dbo.collection("dentists").insertOne(jsonParsed, function (err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+    });
 });
