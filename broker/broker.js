78148c31bee69aebb4a9c189d0e0d6e53e77e5f6
iachella <iachella@student.chalmers.se>
2022-01-03 16:31:35 +0100

    [PAIR] Change local host to remote
    Co-Authored-By: puerto <puerto@student.chalmers.se>

--- broker/broker.js
+++ broker/broker.js
@@ -5,59 +5,16 @@ const database = require('../Database/database');
 const validatorTopic = topics.validatorTopic;
 const frontendTopic = topics.frontendTopic;
 const backendTopic = topics.backendTopic;
-const localHost = 'mqtt://127.0.0.1'; // Local host
-const localPort = ':1883';
 
-const options = {
-    keepalive: 60,
-    protocolId: 'MQTT',
-    protocolVersion: 4,
-    clean: true,
-    reconnectPeriod: 1000,
-    connectTimeout: 30 * 1000,
-    will: {
-        topic: 'WillMsg',
-        payload: 'Connection Closed abnormally..!',
-        qos: 1,
-        retain: false
-    },
-    identifier: 'Local Host',
-    host: (localHost+localPort)
+
+function publish(topic, message) {
+    remoteClient.publish(topic, message, { qos: 1, retain:false });
 }
 
-const client = mqtt.connect(options.host, options);
-
-function publish(topic, message, targetClient) {
-    targetClient.publish(topic, message, { qos: 1, retain:false });
+function subscribe(topic) {
+    remoteClient.subscribe(topic);
+    console.log("Subscribed to: " + topic, { qos: 2 });
 }
-
-function subscribe(topic, targetClient) {
-    targetClient.subscribe(topic);
-    var name = targetClient.options.identifier;
-    console.log(name + " subscribed to: " + topic, { qos: 2 });
-}
-
-client.on('connect', function() {
-    
-    subscribe(validatorTopic, client);
-    //To discuss: for future implementation of occupying timeslot before confirming booking request
-    subscribe(frontendTopic, client);
-    //publish(frontendTopic, 'User request: ...');
-    //TODO: update frontend with real-time available timeslots
-    //publish(validatorTopic, '1', client);
-    //publish(frontendTopic, '1', client);
-})
-
-client.on('message', function(topic, message) {
-
-    if (topic == validatorTopic){
-        appointments.persistAppointment(message);
-    }
-    if (topic == frontendTopic) {
-        database.timeSlots(backendTopic);
-    }
-    console.log('Local host message: ' + message);
-})
 
 const host = "ws://broker.emqx.io"
 const remotePort = ':8083/mqtt';
@@ -75,8 +32,8 @@ const remoteOptions = {
     reconnectPeriod: 1000,
     connectTimeout: 30 * 1000,
     will: {
-        topic: 'WillMsg',
-        payload: 'Connection Closed abnormally..!',
+        topic: 'WillMsg12',
+        payload: 'backend failure',
         qos: 1,
         retain: false
     },
@@ -87,17 +44,14 @@ const remoteOptions = {
 const remoteClient = mqtt.connect(remoteOptions.host, remoteOptions);
 
 remoteClient.on('connect', function() {
-    subscribe(validatorTopic, remoteClient);
-    subscribe(frontendTopic, remoteClient);
-    subscribe(backendTopic, remoteClient);
-    publish(frontendTopic, '1', remoteClient);
+    subscribe(validatorTopic);
+    subscribe(frontendTopic);
 });
 
 remoteClient.on('message', function(topic, message) {
     if (topic == frontendTopic) {
-        database.timeSlots(backendTopic, remoteClient);
+        //database.timeSlots(backendTopic);
     }
-    //console.log(JSON.parse(message));
 })
 
 exports.publish = publish;
