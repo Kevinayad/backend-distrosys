d2a60cf3430badaea9f5d29094fee292b095b913
iachella <iachella@student.chalmers.se>
2022-01-08 15:24:32 +0100

    send only available times

--- broker/broker.js
+++ broker/broker.js
@@ -46,7 +46,6 @@ const remoteClient = mqtt.connect(remoteOptions.host, remoteOptions);
 remoteClient.on('connect', function() {
     subscribe(validatorTopic);
     subscribe(frontendTopic);
-    publish(frontendTopic, '1');
 });
 
 remoteClient.on('message', function(topic, message) {
